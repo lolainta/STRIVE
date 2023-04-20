395d5bd549f35775f16fcc49fa709c61f4cf5ba6
LolaInTa Chi <lolainta@gmail.com>
2023-04-21 00:09:30 +0800

    perf: multi process

--- src/generation/main.py
+++ src/generation/main.py
@@ -1,50 +1,61 @@
 import argparse
 import os
 import pickle
-from tqdm import trange, tqdm
 from nuscenes.nuscenes import NuScenes
 from Generator import Generator
 from NuscData import NuscData
+from multiprocessing import Process
+
+
+def gen_scene(nusc, idx, args):
+    nuscData: NuscData = NuscData(nusc, idx)
+    gen: Generator = Generator(nuscData)
+    dataCluster = gen.gen_all()
+    validData = [ds for ds in dataCluster if ds.filter() == True]
+    osz = len(dataCluster)
+    sz = len(validData)
+    fsz = osz - sz
+    if args.verbose:
+        print(f"scene[{idx}]: {osz}-{fsz}={sz} data generated")
+    if args.record and sz > 0:
+        scene_dir = os.path.join(args.record_path, dataCluster[0].scene["token"])
+        os.makedirs(scene_dir, exist_ok=True)
+        for dataset in dataCluster:
+            with open(
+                os.path.join(scene_dir, f'{dataset.inst["token"]}.pickle'), "wb"
+            ) as f:
+                pickle.dump(dataset, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def run(args):
-    nusc = NuScenes(version=args.dataset, dataroot="./data", verbose=args.verbose)
-    cnt = 0
-    print = tqdm.write
+    nusc = NuScenes(
+        version=args.dataset,
+        dataroot="data/nuscenes/trainval/",
+        verbose=args.verbose,
+    )
+    plist = list()
+    for i in range(len(nusc.scene)):
+        p = Process(target=gen_scene, args=(nusc, i, args))
+        plist.append(p)
+
     print("Generating data...")
-    for i in trange(len(nusc.scene)):
-        nuscData: NuscData = NuscData(nusc, i)
-        gen: Generator = Generator(nuscData)
-        dataCluster = gen.gen_all()
-        validData = [ds for ds in dataCluster if ds.filter() == True]
-        osz = len(dataCluster)
-        sz = len(validData)
-        fsz = osz - sz
-        cnt += sz
-        if args.verbose:
-            print(f"scene[{i}]: {osz}-{fsz}={sz} data generated")
-        if args.record:
-            scene_dir = os.path.join(args.record_path, dataCluster[0].scene["token"])
-            os.makedirs(scene_dir, exist_ok=True)
-            for dataset in dataCluster:
-                with open(
-                    os.path.join(scene_dir, f'{dataset.inst["token"]}.pickle'), "wb"
-                ) as f:
-                    pickle.dump(dataset, f, protocol=pickle.HIGHEST_PROTOCOL)
+    for t in plist:
+        t.start()
+    for t in plist:
+        t.join()
     print("======")
-    print(f"{cnt} data generated from {len(nusc.scene)} scenes")
 
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="python3 src/main.py",
+        prog="python3 src/generation/main.py",
         description="Generate data from given nuscene dataset and collision type",
     )
     parser.add_argument(
         "-d",
         "--dataset",
         choices=["v1.0-mini", "v1.0-trainval"],
-        default="v1.0-mini",
+        default="v1.0-trainval",
         help="Nuscene dataset version",
     )
     parser.add_argument(
